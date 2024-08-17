b1ec3cb13761dc14eceddafcaad2b163b6394bcf
lakatom <laercioat@hotmail.com>
2024-08-17 00:39:38 +0000

    alterar a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>Oi Julia e Theo, vocês estão na Internet!!!</h1>
+    return <h1>Oi Julia e Theo, vocês estão na Internet!!! Querem escrever para aparecer aqui?</h1>
 }
 export default Home;
